0a3517dd28742d2ad138b40ce631c54f89e083df
WebDevProject35 <webdevproject35@gmail.com>
2021-12-08 00:18:36 +0530

    half code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("initial commit2")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded ({ extended: true}));
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+app.listen(3000, function() {
+    console.log("Server is started");
+})
